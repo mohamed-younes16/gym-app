7843bf67dddb3847fdb83a18ba8d5ea11a1226b3
mohamed younes <bettiouim77@gmail.com>
2023-08-26 16:48:32 +0100

    second  commit

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,9 +9,9 @@ return (
     <div className="flex flex-col items-center  mt-10 py-24 border-t-4">
 
         <div className="flex gap-10 mb-8">
-            <a href=""><img src={git} alt="git logo" className=" h-10"/></a>
-            <a href=""><img src={linked} alt="linkedin logo"  className=" h-10"/></a>  
-            <a href=""><img src={ig} alt="instagram logo " className=" h-10" /></a>    
+            <a href="https://github.com/mohamed-younes16"><img src={git} alt="git logo" className=" h-10"/></a>
+            <a href="https://www.linkedin.com/in/mohamed-younes-bettioui-7a07b8271/"><img src={linked} alt="linkedin logo"  className=" h-10"/></a>  
+            <a href="https://www.instagram.com/younesmohamed_77/"><img src={ig} alt="instagram logo " className=" h-10" /></a>    
         </div>
         <img src={logo} alt="" className=" w-40" />
     </div>
--- src/components/Testimo.jsx
+++ src/components/Testimo.jsx
@@ -3,33 +3,38 @@ import { testimonialsData } from "../data/testimonialsData"
 import arri from "../assets/rightArrow.png"
 import arle from "../assets/leftArrow.png"
 import { motion } from "framer-motion"
+import Animated from "./Animated"
 export default function Testimo() {
     const [selected ,setselected ] = useState(0)
 const len = testimonialsData.length
 return (
     <div className="flex mt-24 max-md:flex-col" id="testi">
-        <div className=" flex-3">
+
+        <motion.div className=" flex-3"  >
         <p className="text-orange-400 font-bold">TESTIMONIALS</p>
         <div className="text-[3rem]  font-bold ">
             <span className="clip-text">WHAT THEY</span> 
             <p>SAY ABOUT US</p>
         </div>
-        <p className=" my-6 text-lg font-medium leading-[50px]  "> {testimonialsData[selected].review}  </p>
+        <Animated className=" flex-3"  key={selected}>
+            <p className=" my-6 text-lg font-medium leading-[50px]  "> {testimonialsData[selected].review}  </p>
+
+
+        <div className="flex gap-2 mb-10">
+            <div className="text-orange-400">{testimonialsData[selected].name } </div> - <p> {testimonialsData[selected].status}</p> 
+        </div></Animated>
+        
+        
         <div className="flex justify-end gap-3"> 
             <button><img src={arle} alt="" className="h-6 " onClick={()=> setselected(s =>(len+(s-1 )) % len)} /></button>
             <button><img src={arri} alt="" className="h-6 "  onClick={()=> setselected(s =>(s+1) % len)}  /></button>
         </div>
-        <div className="flex gap-2 mb-10">
-            <div className="text-orange-400">{testimonialsData[selected].name } </div> - <p> {testimonialsData[selected].status}</p> 
-        </div>
-        
-        
-        </div>
+        </motion.div>
 
         <div className="img-wrap flex-2 relative">
             <motion.div   initial={{x:90}} animate={{x:0}} transition={{duration:.7 , ease:"easeIn"}}   className="before" ></motion.div>
             <motion.div   initial={{x:-90}} animate={{x:0}} transition={{duration:.7 , ease:"easeIn" }}   className="after"></motion.div>
-            <img  src={testimonialsData[selected].image} className="max-md:relative max-md:right-auto z-10 top-[10%] right-[15%] absolute h-[320px] w-[270px] object-cover" alt="" />
+            <motion.img key={selected} initial={{opacity:0,y:75}} animate={{opacity:1,y:0}}  transition={{duration:.7}}  src={testimonialsData[selected].image} className="max-md:relative max-md:right-auto z-10 top-[10%] right-[15%] absolute h-[320px] w-[270px] object-cover" alt="" />
         </div>
     </div>
     
